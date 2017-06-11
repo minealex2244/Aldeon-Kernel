ca2e58e44f290908255c341665dcf29762cce125
minealex2244 <minealex2244@gmail.com>
2017-06-11 22:19:10 +0300

    Source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 10 20:46:46 EEST 2017"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "minealex2244-desktop"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
