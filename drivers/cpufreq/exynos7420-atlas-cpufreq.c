242aa4cde43e9dc352270d81492cd835f83f13d4
minealex2244 <minealex2244@gmail.com>
2017-06-22 22:30:00 +0300

    Fixed freezes and random reboots from OC by setting the max freq to 2.4 GHz

--- drivers/cpufreq/exynos7420-atlas-cpufreq.c
+++ drivers/cpufreq/exynos7420-atlas-cpufreq.c
@@ -384,7 +384,7 @@ static void __init set_volt_table_CA57(void)
 	case 5 :
 		max_support_idx_CA57 = L10; break;	/* 1.5GHz */
 	default :
-		max_support_idx_CA57 = L0;		/* 2.5GHz */
+		max_support_idx_CA57 = L1;		/* 2.4GHz */
 	}
 #else
 	max_support_idx_CA57 = L13;	/* 1.2 GHz */
