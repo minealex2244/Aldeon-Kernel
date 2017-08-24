1d70600f845f06b9effafaa065ea2b87d7205645
minealex2244 <minealex2244@gmail.com>
2017-08-24 17:44:34 +0300

    Worked on some Synapse files

--- build_kernel/G920F/ramdisk/sbin/aldeon.sh
+++ build_kernel/G920F/ramdisk/sbin/aldeon.sh
@@ -1,11 +1,24 @@
 #!/system/bin/sh
 
+# Check the location of BusyBox
+
+if [ ! -f /su/xbin/busybox ]; then
+	BB=/system/xbin/busybox;
+else
+	BB=/su/xbin/busybox;
+fi;
+
 # KNOX warranty status
 sh /sbin/resetprop.sh > /dev/null;
 
 # Mount rootfs as RW
 
 mount -o rw,remount rootfs;
+
+# Fix permissions
+
+chown system system /sys/devices/system/cpu/cpu4/cpufreq/interactive/param_index
+chmod 0660 /sys/devices/system/cpu/cpu4/cpufreq/interactive/param_index
 
 # Synapse
 
